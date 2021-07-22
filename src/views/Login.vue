b7ab34961878ae35f2e20ff052ca57b4717497b9
maliqazzz <malikazizhabibie@gmail.com>
2021-07-22 16:55:07 +0700

    22 juli

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -23,31 +23,31 @@
       <v-container class="kolomtxt">
         <v-card class="d-flex flex-column" width="100%" color="transparent" flat>
           <!-- <form @submit.prevent="handleSubmit"> -->
-            <v-text-field outlined dark dense full-width required class="mb-4" label="username" hide-details="auto" v-model="username" prepend-icon="mdi-account" :rules="rules" v-on:click="messagemsg = ''"/>
+            <v-text-field outlined dark dense full-width required class="usr mb-4" label="username" hide-details="auto" v-model="username" prepend-icon="mdi-account" :rules="rules" v-on:click="messagemsg = ''"/>
         
-            <v-text-field outlined dark dense full-width class="mb-6" label="password" hide-details="auto" prepend-icon="mdi-lock" v-model="password" required :rules="rulespassword" :type="typetext" :append-icon="icontext" @click:append="(e)=>handleappend(e)" :error-messages="messagemsg == 'User or Password not match' ? 'User or Password not match' : ''" v-on:click="messagemsg = ''"/>
+            <v-text-field outlined dark dense full-width class="psw mb-6" label="password" hide-details="auto" prepend-icon="mdi-lock" v-model="password" required :rules="rulespassword" :type="typetext" :append-icon="icontext" @click:append="(e)=>handleappend(e)" :error-messages="messagemsg == 'User or Password not match' ? 'User or Password not match' : ''" v-on:click="messagemsg = ''"/>
             
-            <v-btn style="width:100%;" type="submit" v-on:click="validate" :disabled="username == '' || password == '' ? true : false">Submit</v-btn>
+            <v-btn style="width:100%;" class="mb-2" type="submit" v-on:click="validate" :disabled="username == '' || password == '' ? true : false">Submit</v-btn>
             
-            <v-btn style="width:100%;" v-on:click="dialog=true">Register</v-btn>
+            <v-btn style="width:100%;" class="mb-4" v-on:click="dialog=true">Register</v-btn>
 
             <v-dialog v-model="dialog"  max-width="50%" >
-                <v-card>
-                    <v-col>
-                        <v-text-field label="username" :rules="rulesusernameregister()" hide-details="auto" v-model="usernameregister"/>
-                    </v-col>
-                    <v-col>
-                        <v-text-field label="password" :rules="rulespassword" hide-details="auto" v-model="passwordregister" type="password"/>
-                    </v-col>
-                    <v-col>
-                        <v-text-field label="re-enter password" :rules="rulespasswordregister()" hide-details="auto" v-model="passwordregisterreenter" type="password"/>
-                    </v-col>
-                    <v-col style="float:center;position:relative">
-                        <v-btn v-on:click="register()" style="left:50%; position:relative; top:50%; transform:translate(-50%); width:90%" >
-                            Submit
-                        </v-btn>
-                    </v-col>
-                </v-card>
+              <v-card>
+                  <v-col>
+                      <v-text-field label="username" :rules="rulesusernameregister()" hide-details="auto" v-model="usernameregister"/>
+                  </v-col>
+                  <v-col>
+                      <v-text-field label="password" :rules="rulespassword" hide-details="auto" v-model="passwordregister" type="password"/>
+                  </v-col>
+                  <v-col>
+                    <v-text-field label="re-enter password" :rules="rulespasswordregister()" hide-details="auto" v-model="passwordregisterreenter" type="password"/>
+                  </v-col>
+                  <v-col style="float:center;position:relative">
+                    <v-btn v-on:click="register()" style="left:50%; position:relative; top:50%; transform:translate(-50%); width:90%" >
+                        Submit
+                    </v-btn>
+                  </v-col>
+              </v-card>
             </v-dialog>
             <v-dialog v-model="message" persistent max-width="177px" >
                 <v-card>
@@ -234,5 +234,12 @@ export default {
     position: absolute;
   }
 
+  .usr {
+    position: absolute;
+  }
+
+  .psw {
+    position: absolute;
+  }
 
 </style>
